738272c8b6ce730e6dd3284710179318e506b81b
TutiTuti <cys980220@gmail.com>
2023-07-26 01:02:33 +0900

    1011

---
+++ 1011_Fly_me_to_the_Alpha_Centauri.py
@@ -0,0 +1,17 @@
+import sys
+inputversion = "4"
+sys.stdin = open(".\input\input_1011.txt", "r")
+
+
+
+
+for T in range(int(input())):
+    x, y = map(int, input().split()) # 0 <= x < y < 2^31
+    distance = (y - x)
+    sqrt = round(distance ** 0.5)
+
+    if (distance - sqrt ** 2) > 0:
+        print(2*sqrt)
+        continue
+    print(2*sqrt - 1)
+
