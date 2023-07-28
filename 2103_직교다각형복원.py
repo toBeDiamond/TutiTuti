55d9f4942c8ed6fcb3eb44e22a7034832c697d23
최용수 <cys980220@gmail.com>
2023-07-28 17:54:52 +0900

    a

--- 2103_직교다각형복원.py
+++ 2103_직교다각형복원.py
@@ -3,16 +3,58 @@ sys.stdin = open(".\input\input_2103.txt", "r")
 
 
 
+input = sys.stdin.readline
 
 dot_point = [] # 점들을 담을 리스트
-dot_point11 = [1,2,1,2]
-for T in range(int(input())): # 점 개수 T를 받아 반복문 실행
+result = 0
+
+T = int(input())
+x_list= [0] * T
+y_list= [0] * T
+y_list=dot_point[:]
+for T in range(T): # 점 개수 T를 받아 반복문 실행
     dot_point.append(list(map(int,input().split()))) # 점의 좌표를 리스트에 추가한다.
-print(dot_point)
+
+
+x_list=dot_point[:]
+y_list=dot_point[:]
+
 for i in range(len(dot_point)): #점 x좌표 기준 정렬
     for j in range(len(dot_point) - i - 1):
-        if dot_point[j][0] > dot_point[j+1][0]:
-            dot_point[j], dot_point[j+1] = dot_point[j+1], dot_point[j]
+        if x_list[j][0] > x_list[j+1][0]:
+            x_list[j], x_list[j+1] = x_list[j+1], x_list[j]
+        if y_list[j][1] > y_list[j+1][1]:
+            y_list[j], y_list[j+1] = y_list[j+1], y_list[j]
 
-print(dot_point)
+for i in range(0,T,2):
+    result += abs(x_list[i][1] - x_list[i+1][1]) + abs(y_list[i][0] - y_list[i + 1][0])
+
+print(result)
+
+
+
+# import sys
+# sys.stdin = open("input.txt", "r")
+#
+#
+# T = int(input())
+# x_dict = {}
+# y_dict = {}
+# result = 0
+#
+# for i in range(T):
+#     a, b = map(int ,input().split())
+#     if x_dict.get(a, -1) != -1:
+#         result += abs(x_dict.pop(a) - b)
+#     else:
+#         x_dict[a] = b
+#
+#     if y_dict.get(b, -1) != -1:
+#         result += abs(y_dict.pop(b) - a)
+#     else:
+#         x_dict[b] = a
+#
+#
+# print(result)
+
 
